404e2d522e8d1776ca390c0b079a973def0c7c7b
Adrian Smith <smith.adriane@gmail.com>
2025-01-07 12:23:50 -0800

    another try at a macports compatible jar.

--- bindings/java/fix.sh
+++ bindings/java/fix.sh
@@ -11,6 +11,10 @@ set -x
 # install_name_tool -change /Users/runner/work/manifold/manifold/build/_deps/freetype2-build/libfreetype.6.dylib '@rpath/libfreetype.6.dylib' libmeshIO.dylib
 
 
-install_name_tool -change /opt/homebrew/opt/assimp/lib/libassimp.5.dylib '@rpath/libassimp.5.dylib' target/classes/libmanifold.3.0.0.dylib 
-install_name_tool -change /opt/homebrew/opt/libpng/lib/libpng16.16.dylib '@rpath/libpng16.16.dylib' target/classes/libmanifold.3.0.0.dylib 
-install_name_tool -change /opt/homebrew/opt/harfbuzz/lib/libharfbuzz.0.dylib '@rpath/libharfbuzz.0.dylib' target/classes/libmanifold.3.0.0.dylib 
+# install_name_tool -change /opt/homebrew/opt/assimp/lib/libassimp.5.dylib '@rpath/libassimp.5.dylib' target/classes/libmanifold.3.0.0.dylib 
+# install_name_tool -change /opt/homebrew/opt/libpng/lib/libpng16.16.dylib '@rpath/libpng16.16.dylib' target/classes/libmanifold.3.0.0.dylib 
+# install_name_tool -change /opt/homebrew/opt/harfbuzz/lib/libharfbuzz.0.dylib '@rpath/libharfbuzz.0.dylib' target/classes/libmanifold.3.0.0.dylib 
+
+install_name_tool -change /opt/homebrew/opt/assimp/lib/libassimp.5.dylib '/opt/local/lib/libassimp.5.dylib' target/classes/libmanifold.3.0.0.dylib 
+install_name_tool -change /opt/homebrew/opt/libpng/lib/libpng16.16.dylib '/opt/local/lib/libz.1.dylib' target/classes/libmanifold.3.0.0.dylib 
+install_name_tool -change /opt/homebrew/opt/harfbuzz/lib/libharfbuzz.0.dylib '/opt/local/lib/libharfbuzz.0.dylib' target/classes/libmanifold.3.0.0.dylib 
